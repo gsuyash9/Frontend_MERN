3f0bae33697c02629d434c6d80b393c4f0e25343
gsuyash9 <2018256@iiitdmj.ac.in>
2021-03-30 21:07:58 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import react, { Component } from 'react'
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './Components/MenuComponent.js'
 import './App.css';
+import { DISHES } from './shared/dishes.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar darkcolor="primary">
+          <div className='Container'>
+            <NavbarBrand href='/'>Ristorente de confusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
